f6eaf4eb554249f8fa0b06a72ed8a6997130968a
Guillaume Cusnieux <guillaume.cusnieux@ineat-conseil.fr>
2012-07-26 17:28:31 +0200

    prepare tag 1.0

--- samples/play20-qunit-sample/project/Build.scala
+++ samples/play20-qunit-sample/project/Build.scala
@@ -5,11 +5,11 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "play20-qunit-sample"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "1.0"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-        "play20-qunit" % "play20-qunit_2.9.1" % "1.0-SNAPSHOT"
+        "play20-qunit" % "play20-qunit_2.9.1" % "1.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
